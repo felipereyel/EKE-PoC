91fe0109535d2e6eb5d3c11a9c212151aeae8e50
felipe reyel <felipe.reyel@gmail.com>
2021-07-19 23:05:20 -0300

    init crypt

---
+++ python_cli_app/eke.py
@@ -0,0 +1,42 @@
+import secrets
+from tinyec import registry
+from tinyec.ec import Point
+from Crypto.Cipher import AES
+
+curve = registry.get_curve('secp256r1')
+
+class EKE:
+
+    def __init__(self, privKey, otherPub, curve=curve):
+        self.curve = curve
+        self.privKey = privKey
+        self.otherPub = Point(self.curve, otherPub["x"], otherPub["y"])
+
+    def startExchange(self):
+        mySessionSecret = secrets.randbelow(self.curve.field.n)
+        toOther = (self.privKey + mySessionSecret) * self.otherPub
+
+        mySelfPartialShared = self.privKey * self.curve.g
+        self.mySelfPartialShared = mySelfPartialShared
+
+        return toOther
+
+    def finishExchange(self, received):
+        privKeyInv = pow(self.privKey, -1, self.curve.field.n)
+        myPartialShared =  (received * privKeyInv) - self.otherPub
+        shared = self.mySelfPartialShared + myPartialShared
+
+        self.key = shared.x
+        return shared.x
+
+    def crypt(self, msg):
+        cipher = AES.new(self.key, AES.MODE_EAX)
+        nonce = cipher.nonce
+        ciphertext, tag = cipher.encrypt_and_digest(msg)
+        return nonce, ciphertext, tag
+
+    def decrypt(self, nonce, ciphermsg, tag):
+        cipher = AES.new(self.key, AES.MODE_EAX, nonce=nonce)
+        plaintext = cipher.decrypt(ciphermsg)
+        cipher.verify(tag)
+        return plaintext
--- python_cli_app/node.py
+++ python_cli_app/node.py
@@ -1,5 +1,6 @@
 import time, sys
 from p2pnetwork.node import Node
+from eke import EKE
 
 sysCTA = "sys > "
 
@@ -11,18 +12,9 @@ class P2P (Node):
 
         self.nodeInfo = nodeInfo
         self.destInfo = destInfo
+        self.ke = EKE(nodeInfo["private"], destInfo["pub"])
 
         self.start()
-
-    def connect(self):    
-        for i in range(5):
-            time.sleep(1)
-            if self.connect_with_node(self.destInfo["host"], self.destInfo["port"]):
-                break
-            else:
-                continue
-        else:
-            sys.exit(f'Nao conseguiu conectar com usuario {self.destInfo["user"]}')
 
     @property
     def selfCTA(self):
@@ -34,29 +26,49 @@ class P2P (Node):
 
     # Init connection
 
-    def outbound_node_connected(self, node):
-        self.chatPrint(sysCTA + 'outbound_node_connected')
-        
-    def inbound_node_connected(self, node):
-        self.chatPrint(sysCTA + 'inbound_node_connected')
+    def connect(self):
+        for i in range(10):
+            time.sleep(0.5)
+            if self.connect_with_node(self.destInfo["host"], self.destInfo["port"]):
+                break
+            else:
+                continue
+        else:
+            sys.exit(f'Nao conseguiu conectar com usuario {self.destInfo["user"]}')
+        time.sleep(1)
+        # self.startCrypt()
+
+    def startCrypt(self, answer=False):
+        pass
 
     # Message handling
 
     def send_message(self, msg):
         self.chatPrint(self.selfCTA + str(msg))
-        return self.send_to_nodes(msg)
+        return self.send_to_nodes({ "type": "message", "content": msg })
 
     def node_message(self, node, data):
-        self.chatPrint(self.connectedCTA + str(data))
+        if data.get("type") == "message":
+            self.chatPrint(self.connectedCTA + data.get("content"))
+        elif data.get("type") == "config":
+            pass
+        else:
+            self.chatPrint(sysCTA + 'UNKNOWN MESSAGE TYPE')
 
     ## System prints
+
+    def outbound_node_connected(self, node):
+        self.chatPrint(sysCTA + 'outbound_node_connected')
+
+    def inbound_node_connected(self, node):
+        self.chatPrint(sysCTA + 'inbound_node_connected')
 
     def inbound_node_disconnected(self, node):
         self.chatPrint(sysCTA + 'inbound_node_disconnected')
 
     def outbound_node_disconnected(self, node):
         self.chatPrint(sysCTA + 'outbound_node_disconnected')
-        
+
     def node_disconnect_with_outbound_node(self, node):
         self.chatPrint(sysCTA + 'node_disconnect_with_outbound_node')
 
